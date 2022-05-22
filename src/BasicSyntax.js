02ab417fc5063bdbb0f6e459a54cbe0cb9c391c1
grisha160897@mail.ru <grisha160897@mail.ru>
2022-05-22 14:42:07 +0300

    задание по task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,14 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    const alfabet = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    for (let i = 0; i < str.length; i++) {
+        if (alfabet[str[i]] < alfabet[str[i + 1]]) {
+            result += alfabet[str[i + 1]] - alfabet[str[i]];
+            i++;
+        } else {
+            result += alfabet[str[i]];
+        }
+    }
     return result;
 }
